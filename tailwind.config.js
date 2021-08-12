1634b08e23d555532960ab03d144a5fe2603773c
Sander Geraedts <sandergeraedts@live.nl>
2021-08-12 21:43:38 +0200

    Initial setup and working prototype

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,9 @@ module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
   // more options here
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+  },
 };
