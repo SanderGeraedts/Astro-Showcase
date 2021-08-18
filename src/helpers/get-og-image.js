ee1abb351d753ee3ab595f6bc5516a729f29d818
Sander Geraedts <sandergeraedts@live.nl>
2021-08-18 14:31:36 +0200

    Removed local file-based image cache which was not working correctly in favour of using cloudinary. Also made the index.astro use the local api cache.

--- src/helpers/get-og-image.js
+++ src/helpers/get-og-image.js
@@ -1,8 +1,92 @@
 import * as cheerio from "cheerio";
 import fs from "node:fs";
 import getUrl from "./get-url.js";
+import * as cl from "cloudinary";
+import dotenv from "dotenv";
 
-export default async (url) => {
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
+  process.env;
+
+const cloudinary = cl.v2;
+
+cl.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+/**
+ * Calls the 11ty screenshot API to take a screenshot of the given homepage
+ *
+ * @param {string} homepage
+ * @returns Buffer
+ */
+const getScreenshot = async (homepage) => {
+  const response = await fetch(
+    `https://v1.screenshot.11ty.dev/${encodeURIComponent(homepage)}/opengraph`
+  );
+
+  const arrayBuffer = await response.arrayBuffer();
+  return Buffer.from(arrayBuffer);
+};
+
+const uploadScreenshot = (screenshot, filename) => {
+  return new Promise((resolve, reject) => {
+    const uploadOptions = {
+      folder: "astro-showcase",
+      public_id: filename,
+      overwrite: true,
+    };
+    cloudinary.uploader
+      .upload_stream(uploadOptions, (error, result) => {
+        if (error) reject(error);
+        else resolve(result);
+      })
+      .end(screenshot);
+    console.log(`Upload of ${filename} complete!`);
+  });
+};
+
+/**
+ * Checks if the image exists on Cloudinary
+ *
+ * @param {string} filename
+ * @returns image Object or undefined
+ */
+const checkImage = (filename) => {
+  return new Promise((resolve) => {
+    cloudinary.api.resource(`astro-showcase/${filename}`, (error, result) => {
+      if (error && error.http_code === 404) {
+        resolve(undefined);
+      }
+
+      resolve(result);
+    });
+  });
+};
+
+/**
+ * Removes the file from Cloudinary
+ *
+ * @param {string} filename
+ * @returns
+ */
+const destroyImage = (filename) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.destroy(
+      `astro-showcase/${filename}`,
+      (error, result) => {
+        resolve(result);
+      }
+    );
+  });
+};
+
+export default async (url, cloudinaryOptions = "c_fit,h_235,w_448") => {
   const homepage = getUrl(url);
 
   const data = await fetch(homepage);
@@ -15,22 +99,29 @@ export default async (url) => {
   if (imageUrl) {
     return imageUrl.startsWith("http") ? imageUrl : homepage + imageUrl;
   } else {
-    const response = await fetch(
-      `https://v1.screenshot.11ty.dev/${encodeURIComponent(homepage)}/opengraph`
-    );
-    const arrayBuffer = await response.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-
     const filename = url.replace(/(^\w+:|^)\/\//, "").replaceAll("/", "");
+    const image = await checkImage(filename);
+    const cloudinaryUrl = `https://res.cloudinary.com/sandergnl/image/upload/${cloudinaryOptions}/astro-showcase/${filename}.jpg`;
 
-    const cache = "./public/.cache";
+    // If the image does not exist, upload it.
+    // If the image does exist, but is older than 1 day
+    // remove it and reupload it (to reset the created_at date)
+    // If the image does exist, but is younger than 1 day, return the url
+    if (image) {
+      const createdTime = new Date(image.created_at).getTime();
+      const currentTime = new Date().getTime();
 
-    if (!fs.existsSync(cache)) {
-      fs.mkdirSync(cache, { recursive: true });
+      // Check if the image is older than 1 day
+      if (currentTime - createdTime > 24 * 60 * 60 * 1000) {
+        await destroyImage(filename);
+      } else {
+        // else, just return the cloudinaryUrl
+        return cloudinaryUrl;
+      }
     }
+    const buffer = await getScreenshot(homepage);
+    await uploadScreenshot(buffer, filename);
 
-    fs.createWriteStream(`./public/.cache/${filename}.png`).write(buffer);
-
-    return `/.cache/${filename}.png`;
+    return cloudinaryUrl;
   }
 };
--- src/helpers/get-projects.js
+++ src/helpers/get-projects.js
@@ -1,9 +1,36 @@
-export default (projects) => {
-  return projects.filter(
-    (project) =>
-      project.homepage &&
-      !project.homepage.includes("localhost") &&
-      !project.name.includes("-issue") &&
-      !project.name.includes("repro")
-  );
+import fs from "node:fs";
+
+export default async () => {
+  const cache = "./public/.cache";
+
+  if (!fs.existsSync(cache)) {
+    fs.mkdirSync(cache, { recursive: true });
+  }
+
+  // Check if "caching" file exists
+  if (fs.existsSync("./public/.cache/local.json")) {
+    // Read data from file
+    const raw = fs.readFileSync("./public/.cache/local.json");
+    return JSON.parse(raw);
+  } else {
+    // Make API call and write to file
+    const response = await fetch(
+      "https://api.github.com/search/repositories?q=language:Astro&per_page=100"
+    );
+    const data = await response.json();
+
+    // Filter out null homepages and homepages linking to localhost
+    const projects = data.items.filter(
+      (project) =>
+        project.homepage &&
+        !project.homepage.includes("localhost") &&
+        !project.name.includes("-issue") &&
+        !project.name.includes("repro")
+    );
+
+    // Write projects to "caching" file
+    fs.writeFileSync("./public/.cache/local.json", JSON.stringify(projects));
+
+    return projects;
+  }
 };
